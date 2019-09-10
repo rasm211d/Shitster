6f8ba3a9e180cc1d652a275ed9aa5c451ab8bed2
Rasmus Jannerup <54896192+rasm211d@users.noreply.github.com>
2019-09-10 14:51:38 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Shitster 1.0");
+
+        Shitster rasmusLort = new Shitster(); //instansierer et objekt af klassen Shitster
+        rasmusLort.weight = 3;
+        rasmusLort.healthy = false;
+        rasmusLort.temp = 37;
+
+        Shitster henrikLort = new Shitster();
+        henrikLort.weight = 5;
+        henrikLort.healthy = true;
+        henrikLort.temp = 38.5;
+
+        System.out.println("rasmus' afføring vejer: " + rasmusLort.weight + "kg");
+        System.out.print("Rasmus' lort er ");
+        rasmusLort.erDenSund();
+        System.out.println("Rasmus' lort er " + rasmusLort.temp + "grader varm");
+
+        System.out.println();
+
+
+
+    }
+}
---
+++ src/Shitster.java
@@ -0,0 +1,18 @@
+public class Shitster {
+    //Variabler
+    float weight;           //angiver lortens vægt
+    boolean healthy;        //er din lort sund eller ej
+    double temp;             //måler din lorts temperatur
+
+    void erDenSund() {
+        if (healthy) {
+            System.out.println("Sund");
+        }
+        else {
+            System.out.println("Usund");
+        }
+    }
+
+    //Metoder
+
+}
